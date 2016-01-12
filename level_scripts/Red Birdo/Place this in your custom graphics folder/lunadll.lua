792862ee5fbebfbbb378552cac1d68d664ed6c58
Mike Santiago <miketheripper1@gmail.com>
2016-01-11 17:14:48 -1000

    fix red birdo

--- level_scripts/Red Birdo/Place this in your custom graphics folder/lunadll.lua
+++ level_scripts/Red Birdo/Place this in your custom graphics folder/lunadll.lua
@@ -1,25 +1,34 @@
 local hasGenerated = false;
+local hasTransformed = false; --if we've transformed an egg or not
 local ran;
 
 function onLoop()
   tableOfBirdo = NPC.get(39, -1);
   tableOfBirdoEggs = NPC.get(40, -1);
-
   if(tableOfBirdo[1] ~= nil) then
     if(tonumber(tableOfBirdo[1]:mem(0xF0, FIELD_DFLOAT)) == 1) then
+	  frameCount = frameCount + 1;
       if(hasGenerated ~= true) then
         ran = math.random(0, 2);
         hasGenerated = true;
       end
       if(ran == 2) then
-        if(table.getn(tableOfBirdoEggs) > 0) then
-          tableOfBirdoEggs[table.getn(tableOfBirdoEggs)]:mem(0xE2, FIELD_WORD, 282);
-          playSFX(42); --big fireball
+        if(table.getn(tableOfBirdoEggs) > 0) then --checks if eggs exist
+			if(frameCount > 21) then
+				tableOfBirdoEggs = NPC.get(40, -1); --update table
+				if(hasTransformed == false) then
+					tableOfBirdoEggs[table.getn(tableOfBirdoEggs)]:mem(0xE2, FIELD_WORD, 282); --modification
+					playSFX(42); --big fireball
+					hasTransformed = true; --so we don't re-transform anything else
+				end
+			end
         end
       end
     end
     if(tonumber(tableOfBirdo[1]:mem(0xF8, FIELD_DFLOAT)) == 280) then
       hasGenerated = false; --just reset everything
+	  hasTransformed = false;
+	  frameCount = 0;
     end
   end
 end
